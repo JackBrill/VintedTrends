2a50b637f3e275dcf842e0498f06949e818a4263
JackBrill <119537198+JackBrill@users.noreply.github.com>
2025-09-20 18:45:33 +0100

    Update vinted.js

--- vinted.js
+++ vinted.js
@@ -6,7 +6,7 @@ import fetch from "node-fetch";
 import { PROXIES, DISCORD_WEBHOOK_URL, VINTED_CATALOG_URL } from "./config.js";
 
 // Settings
-const BATCH_SIZE = 50; // number of items to track
+const BATCH_SIZE = 75; // number of items to track
 const CHECK_INTERVAL = 60 * 1000; // 60 seconds
 const BATCH_DURATION = 5 * 60 * 1000; // 5 minutes
 
