24644144e552ae245e38edcad9b4df1947799204
JackBrill <119537198+JackBrill@users.noreply.github.com>
2025-09-20 12:49:32 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+// config.js
+
+export const PROXIES = [
+  "208.66.76.70:5994:mtqikwov:autmrqhdcnfn",
+  "72.1.153.25:5417:mtqikwov:autmrqhdcnfn",
+  "150.241.248.232:7449:mtqikwov:autmrqhdcnfn",
+  "138.226.77.124:7313:mtqikwov:autmrqhdcnfn",
+  "45.196.32.223:5855:mtqikwov:autmrqhdcnfn",
+  "45.196.51.227:5923:mtqikwov:autmrqhdcnfn",
+  "46.203.47.181:5680:mtqikwov:autmrqhdcnfn",
+  "62.164.231.66:9378:mtqikwov:autmrqhdcnfn",
+  "45.196.54.120:6699:mtqikwov:autmrqhdcnfn",
+  "46.203.20.40:6541:mtqikwov:autmrqhdcnfn",
+  "154.194.27.8:6548:mtqikwov:autmrqhdcnfn",
+  "104.252.59.11:7483:mtqikwov:autmrqhdcnfn",
+  "45.196.52.181:6196:mtqikwov:autmrqhdcnfn",
+  "46.203.15.117:7118:mtqikwov:autmrqhdcnfn",
+  "45.56.177.51:8852:mtqikwov:autmrqhdcnfn",
+  "46.203.144.26:7793:mtqikwov:autmrqhdcnfn",
+  "130.180.231.36:8178:mtqikwov:autmrqhdcnfn",
+  "104.252.81.67:5938:mtqikwov:autmrqhdcnfn",
+  "104.252.81.97:5968:mtqikwov:autmrqhdcnfn",
+  "154.194.26.109:6350:mtqikwov:autmrqhdcnfn",
+];
+
+export const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1418908479229530223/8_Eg73-4nZL-QoaGKjXIJF0UOxEAMBiuZGRv5CS3VGd11gLvblOgaLNk1UIqmCbTyA6Z";
