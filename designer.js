3e225c000e64d1c93fdfa84e92e30145a212d6cc
JackBrill <119537198+JackBrill@users.noreply.github.com>
2025-09-22 18:54:26 +0100

    Update designer.js

--- designer.js
+++ designer.js
@@ -6,10 +6,10 @@ import fetch from "node-fetch";
 import { PROXIES, DISCORD_WEBHOOK_URL, DESIGNER_URL } from "./config.js";
 
 // Settings
-const BATCH_SIZE = 50; // number of items to track
+const BATCH_SIZE = 75; // number of items to track
 const CHECK_INTERVAL = 60 * 1000; // 60 seconds
-const BATCH_DURATION = 5 * 60 * 1000; // 5 minutes
-const CONCURRENT_CHECKS = 10; // Number of items to check at once
+const BATCH_DURATION = 10 * 60 * 1000; // 5 minutes
+const CONCURRENT_CHECKS = 25; // Number of items to check at once
 const VERBOSE_LOGGING = true;
 
 // Path to sales data
